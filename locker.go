ab1cbd9ea8133a3b525c677af3e549dc93861133
David Ackerman <enfipy@gmail.com>
2018-12-26 22:11:14 +0200

    Remove selflock

--- locker.go
+++ locker.go
@@ -1,112 +1,49 @@
 package locker
 
-import (
-	"fmt"
-	"sync"
-	"sync/atomic"
-)
-
-type lock struct {
-	*sync.RWMutex
-	pending int32
-}
-
-func initLock() *lock {
-	return &lock{
-		RWMutex: new(sync.RWMutex),
-		pending: 0,
-	}
-}
+import "sync"
 
 // Locker type for map of mutexes and self lock to add new mutex to map
 type Locker struct {
-	locks    sync.Map
-	selfLock sync.RWMutex
+	sync.Pool
+	locks sync.Map
 }
 
 // Initialize - return initialized locker struct
 func Initialize() *Locker {
 	return &Locker{
-		locks:    sync.Map{},
-		selfLock: sync.RWMutex{},
+		Pool: sync.Pool{
+			New: func() interface{} {
+				return new(sync.RWMutex)
+			},
+		},
 	}
 }
 
 // Lock - locks mutex
-func (lkr *Locker) Lock(key string) {
-	lk, ok := lkr.getLock(key)
-	if !ok {
-		lk = lkr.newLock(key)
-	}
-	atomic.AddInt32(&lk.pending, 1)
-	lk.Lock()
+func (lkr *Locker) Lock(key interface{}) {
+	lkr.getLock(key).Lock()
 }
 
 // Unlock - unlocks mutex
-func (lkr *Locker) Unlock(key string) {
-	lk, ok := lkr.getLock(key)
-	if !ok {
-		// Todo: Doesn't get here
-		panic(fmt.Errorf("Lock for key '%s' is not initialized", key))
-	}
-	atomic.AddInt32(&lk.pending, -1)
-	lk.Unlock()
-	if lk.pending < 1 {
-		lkr.remLock(key)
-	}
+func (lkr *Locker) Unlock(key interface{}) {
+	lkr.getLock(key).Unlock()
 }
 
 // RLock - locks rw for reading
-func (lkr *Locker) RLock(key string) {
-	lk, ok := lkr.getLock(key)
-	if !ok {
-		lk = lkr.newLock(key)
-	}
-	atomic.AddInt32(&lk.pending, 1)
-	lk.RLock()
+func (lkr *Locker) RLock(key interface{}) {
+	lkr.getLock(key).RLock()
 }
 
 // RUnlock - unlocks a single RLock call
-func (lkr *Locker) RUnlock(key string) {
-	lk, ok := lkr.getLock(key)
-	if !ok {
-		// Todo: Doesn't get here
-		panic(fmt.Errorf("Lock for key '%s' is not initialized", key))
-	}
-	atomic.AddInt32(&lk.pending, -1)
-	lk.RUnlock()
-	if lk.pending < 1 {
-		lkr.remLock(key)
-	}
+func (lkr *Locker) RUnlock(key interface{}) {
+	lkr.getLock(key).RUnlock()
 }
 
-func (lkr *Locker) newLock(key string) *lock {
-	lkr.selfLock.Lock()
-	defer lkr.selfLock.Unlock()
-
-	if lk, ok := lkr.locks.Load(key); ok {
-		return lk.(*lock)
+func (lkr *Locker) getLock(key interface{}) *sync.RWMutex {
+	newLock := lkr.Get()
+	lock, stored := lkr.locks.LoadOrStore(key, newLock)
+	if stored {
+		lkr.Put(newLock)
 	}
-	lk := initLock()
-	lkr.locks.Store(key, lk)
-	return lk
-}
-
-func (lkr *Locker) getLock(key string) (*lock, bool) {
-	lkr.selfLock.RLock()
-	defer lkr.selfLock.RUnlock()
-
-	if lk, ok := lkr.locks.Load(key); ok {
-		return lk.(*lock), ok
-	}
-	return nil, false
-}
-
-func (lkr *Locker) remLock(key string) {
-	lkr.selfLock.Lock()
-	defer lkr.selfLock.Unlock()
-
-	if _, ok := lkr.locks.Load(key); ok {
-		lkr.locks.Delete(key)
-	}
+	return lock.(*sync.RWMutex)
 }
--- locker_test.go
+++ locker_test.go
@@ -7,26 +7,18 @@ import (
 	"time"
 )
 
-func testLock(lkr *Locker, wg *sync.WaitGroup, text string, sleep time.Duration) {
+func testLock(lkr *Locker, wg *sync.WaitGroup, num int, sleep time.Duration) {
 	lkr.Lock("test")
 	time.Sleep(sleep)
-	if lk, ok := lkr.locks.Load("test"); ok {
-		log.Printf("Pending count from '%s': #%d", text, lk.(*lock).pending)
-	} else {
-		log.Fatal("Can not load lock")
-	}
+	log.Printf("Write lock #%d", num)
 	lkr.Unlock("test")
 	wg.Done()
 }
 
-func testRLock(lkr *Locker, wg *sync.WaitGroup, text string, sleep time.Duration) {
+func testRLock(lkr *Locker, wg *sync.WaitGroup, num int, sleep time.Duration) {
 	lkr.RLock("test")
 	time.Sleep(sleep)
-	if lk, ok := lkr.locks.Load("test"); ok {
-		log.Printf("Pending count from '%s': #%d", text, lk.(*lock).pending)
-	} else {
-		log.Fatal("Can not load lock")
-	}
+	log.Printf("Read lock #%d", num)
 	lkr.RUnlock("test")
 	wg.Done()
 }
@@ -40,7 +32,7 @@ func TestLocker(_ *testing.T) {
 		var wg sync.WaitGroup
 		for i := 0; i < 5; i++ {
 			wg.Add(1)
-			go testLock(lkr, &wg, "Mr. Writelock", 100*time.Millisecond)
+			go testLock(lkr, &wg, i, 100*time.Millisecond)
 		}
 		wg.Wait()
 		waitForTests.Done()
@@ -51,7 +43,7 @@ func TestLocker(_ *testing.T) {
 		var wg sync.WaitGroup
 		for i := 0; i < 5; i++ {
 			wg.Add(1)
-			go testRLock(lkr, &wg, "Mr. Readlock", 100*time.Millisecond)
+			go testRLock(lkr, &wg, i, 100*time.Millisecond)
 		}
 		wg.Wait()
 		waitForTests.Done()
